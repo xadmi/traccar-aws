9879d0c81d5a36338f0415f912f873be7cd86790
Łukasz Pinkosz <luk@chybie.net>
2019-12-26 12:07:56 +0100

    fix

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ sudo yum -y update
 sudo yum -y install python2-pip
 sudo pip install ansible==2.8.7
 
-cd /tmp/traccar-aws && sudo ansible-playbook --connection=local -i "localhost," \
+sudo ansible-playbook --connection=local -i "localhost," \
                                          -e "SUBDOMAIN=${SUBDOMAIN}" \
                                          -e "DOMAIN=${DOMAIN}" \
                                          -e "EMAIL=${EMAIL}" \
-                                         install-traccar.yml
+                                         ansible/install-traccar.yml
