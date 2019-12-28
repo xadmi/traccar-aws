fa20dafd453d13a50b22eec2b87e736db4809f1d
Łukasz Pinkosz <luk@chybie.net>
2019-12-28 12:16:38 +0100

    fix

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ sudo yum -y update
 sudo yum -y install python2-pip
 sudo pip install ansible==2.8.7
 
-sudo ansible-playbook --connection=local -i "localhost," ansible/prepare-server.yml
-sudo ansible-playbook --connection=local -i "localhost," ansible/install-traccar.yml
+ansible-playbook --connection=local -i "localhost," ansible/prepare-server.yml
+ansible-playbook --connection=local -i "localhost," ansible/install-traccar.yml
 
 if [[ $TC_LETSENCRYPT == true ]]; then
-  sudo ansible-playbook --connection=local -i "localhost," ansible/install-letsencrypt.yml
+  ansible-playbook --connection=local -i "localhost," ansible/install-letsencrypt.yml
 fi
