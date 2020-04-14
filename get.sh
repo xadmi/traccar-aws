af95ca6ed2b2b12cc455e6b0430efb1061ad49d4
xadmi <luk@chybie.net>
2020-04-14 18:13:26 +0200

    Update get.sh
    github migration

--- get.sh
+++ get.sh
@@ -18,5 +18,5 @@ fi
 yum -y install git
 
 rm -rf /tmp/traccar-aws
-git clone --depth 1 -b master https://bitbucket.org/xadmi/traccar-aws.git /tmp/traccar-aws
+git clone --depth 1 -b master https://github.com/xadmi/traccar-aws.git /tmp/traccar-aws
 cd /tmp/traccar-aws && ./install.sh
